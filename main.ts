0f8f34fb754d9ecd257729608f9128fdd93270a9
Leonardo <leonardocregis@gmail.com>
2017-08-23 16:58:31 -0300

    general refatoring to typescript and post method
    typescript types addiction, returning for post and get

--- main.ts
+++ main.ts
@@ -1,18 +1,36 @@
 let express = require("express");
 let router = express.Router();
-let definitions = require("./definitions");
+let definitions:Definitions = require("./definitions");
+const port = 3004;
 
+interface RouteDef{
+    'url': string;
+    'response': Object;
+    'method': string;
+}
+interface Definitions {
+    routesDef: RouteDef[];
+};
 
-definitions.routes.forEach(route => {
-    router.get(route.url, function (req,res){
-        res.json('hello world');
-    });
-})
-
+definitions.routesDef.forEach(route => {
+    console.log('defining route URL: "'+route.url+'"');
+    if (route.method == 'get'){
+        console.log('defining route URL methdo: "'+route.method+'"');
+        router.get(route.url, function (req,res){
+            res.json(route.response);
+        });
+    };
+    if (route.method == 'post'){
+        console.log('defining route URL methdo: "'+route.method+'"');
+        router.post(route.url, function (req,res){
+            res.json(route.response);
+        });
+    };
+});
 let app = express();
 app.use('/', router);
 
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(port, function () {
+  console.log('App listening on port '+port+' !')
 });
