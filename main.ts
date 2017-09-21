da8b63bb21e36915330a9d7553f54e1ce425891e
Leonardo <leonardocregis@gmail.com>
2017-09-21 14:56:13 -0300

    added a status configuration

--- main.ts
+++ main.ts
@@ -1,35 +1,44 @@
 let express = require("express");
 let router = express.Router();
 let definitions:Definitions = require("./definitions");
-const port = 3004;
+let port = 3004;
 
 interface RouteDef{
     'url': string;
     'response': Object;
     'method': string;
+    'statusCode': number;
 }
 interface Definitions {
+    'port': number;
     routesDef: RouteDef[];
 };
 
 definitions.routesDef.forEach(route => {
     console.log('defining route URL: "'+route.url+'"');
     if (route.method == 'get'){
-        console.log('defining route URL methdo: "'+route.method+'"');
+        console.log('defining route URL methdo: "' + route.method + '"');
         router.get(route.url, function (req,res){
             res.json(route.response);
         });
     };
     if (route.method == 'post'){
-        console.log('defining route URL methdo: "'+route.method+'"');
+        console.log('defining route URL methdo: "' + route.method + '"' + 'status:'+ route.statusCode);
         router.post(route.url, function (req,res){
-            res.json(route.response);
+            if (route.statusCode){
+                res.status(route.statusCode).json(route.response);
+            } else {
+                res.status(200).json(route.response);
+            }
         });
     };
 });
 let app = express();
 app.use('/', router);
 
+if ( definitions.port) {
+    port = definitions.port;
+}
 
 app.listen(port, function () {
   console.log('App listening on port '+port+' !')
