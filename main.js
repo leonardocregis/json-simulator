bdb3c95553f74189dd2dbd924e19092e6621c5f2
Leonardo <leonardocregis@gmail.com>
2017-09-22 07:58:06 -0300

    update based in the ts convertion

--- main.js
+++ main.js
@@ -1,14 +1,36 @@
 "use strict";
-var express = require("express");
-var router = express.Router();
-var definitions = require("./definitions");
-definitions.routes.forEach(function (route) {
-    router.get(route.url, function (req, res) {
-        res.json(route.response);
-    });
+let express = require("express");
+let router = express.Router();
+let definitions = require("./definitions");
+let port = 3004;
+;
+definitions.routesDef.forEach(route => {
+    console.log('defining route URL: "' + route.url + '"');
+    if (route.method == 'get') {
+        console.log('defining route URL methdo: "' + route.method + '"');
+        router.get(route.url, function (req, res) {
+            res.json(route.response);
+        });
+    }
+    ;
+    if (route.method == 'post') {
+        console.log('defining route URL methdo: "' + route.method + '"' + 'status:' + route.statusCode);
+        router.post(route.url, function (req, res) {
+            if (route.statusCode) {
+                res.status(route.statusCode).json(route.response);
+            }
+            else {
+                res.status(200).json(route.response);
+            }
+        });
+    }
+    ;
 });
-var app = express();
+let app = express();
 app.use('/', router);
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+if (definitions.port) {
+    port = definitions.port;
+}
+app.listen(port, function () {
+    console.log('App listening on port ' + port + ' !');
 });
